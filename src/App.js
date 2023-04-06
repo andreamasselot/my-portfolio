da3bb6f0a02f7c2aa2b261959152e0f5446b0740
Andréa Masselot <andrea.nicolas128@gmail.com>
2023-04-06 10:00:37 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
