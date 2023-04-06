4708d6cec1fdf27ecdabcd5479be5f634afa690f
Andréa Masselot <andrea.nicolas128@gmail.com>
2023-04-06 10:29:27 +0200

    home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./styles/Home.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
+import banner from "../assets/img/home-banner.jpg";
 const Home = () => {
   return (
     <>
-      <p>Home</p>
+      <div className="homepage-container">
+        <div className="home-title">
+          <h1>Andréa Masselot</h1>
+          <h2>Web Developer</h2>
+          <h3>Javascript, React.js, Node.js</h3>
+          <button>Next</button>
+        </div>
+        <section className="homepage-body">
+          <div className="banner-image">
+            <img src={banner} alt="illustration" />
+          </div>
+        </section>
+      </div>
     </>
   );
 };
